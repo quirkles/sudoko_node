29a5fd7eee7c5deaafc5b94b5cfa67e815bb7d44
quirkles <alexquirk@joinsherpa.com>
2023-08-12 22:21:57 -0400

    add comments

--- src/Game.ts
+++ src/Game.ts
@@ -58,15 +58,12 @@ export class Game {
         const { i, j, box: b } = cell.locators;
         this.eliminateByGroups(
           unsolvedArr.filter((cell) => !cell.isSolved && cell.isInRow(j)),
-          { type: "row", identifier: i },
         );
         this.eliminateByGroups(
           unsolvedArr.filter((cell) => !cell.isSolved && cell.isInColumn(i)),
-          { type: "col", identifier: j },
         );
         this.eliminateByGroups(
           unsolvedArr.filter((cell) => !cell.isSolved && cell.isInBox(b)),
-          { type: "box", identifier: b },
         );
       }
       const newTotalPossibleValues = this.getTotalPossibleValues();
@@ -100,22 +97,31 @@ export class Game {
     }
   }
 
-  eliminateByGroups(
-    cells: Cell[],
-    meta: { type: "row" | "col" | "box"; identifier: number },
-  ) {
+  eliminateByGroups(cells: Cell[]) {
     this.handleCanOnlyBeInGroup(cells);
-    this.excludeViaExhaustiveBuckets(cells, meta);
+    this.excludeViaExhaustiveBuckets(cells);
   }
 
-  excludeViaExhaustiveBuckets(
-    cells: Cell[],
-    meta: { type: "row" | "col" | "box"; identifier: number },
-  ) {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const vs = cells.map((c) => `${c.valueString}`).join(", ");
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const metaStr = `${meta.type}:${meta.identifier}`;
+  excludeViaExhaustiveBuckets(cells: Cell[]) {
+    // This takes a family of cells (all in the same row, col, or box) and eliminates possible values by creating groups, or buckets.
+    // This is the general case, or which 'handleCanOnlyBeInGroup' is a special case (group size limited to one)
+    // The principle is that if we have 5 cells and their possible values
+
+    // cell 1 | call 2 | cell 3 | cell 4  | cell 5
+    // 1,2,3  | 1,2    | 2,3    | 2,3,7,8 | 1,7,8
+
+    // From this we know that the values 123 are taken up by the first three cells
+    // Therefore we can eliminate 1,2,3 from the possible values  cells 4 and 5
+    // Think of '123' as a bucket into which we can put the cells that exhaust this combination of possible values
+
+    // Another way of thinking about it:
+    // We are going to take EVERY possible combination of 2 or more cells, and for each combination, list all possible values combined
+    // if a combination of two buckets b1 and b2 only has two possible values; x and y, then we know b1 is x or y and b2 is x or y
+    // Therefore we can remove x and y from the possible values of any other buckets in the family
+    // This holds in general, if a bucket containing x cells has x possible values than that is exhaustive, no other cells in the family have those values
+    // Think of the simplest case, is a bucket of 1 cell has only 1 possible value, none of the other buckets have that value
+
+    // First lets get the 'first order' buckets,for each cell, create a bucket from the possible values and put the cell in it, there may be more than one cell in any bucket here
     const firstOrderBuckets: Record<string, Cell[]> = cells.reduce(
       (acc, cell) => {
         const valueString = cell.valueString;
@@ -129,11 +135,21 @@ export class Game {
       {} as Record<string, Cell[]>,
     );
 
+    // The composite bucket will contain all possible combinations
     const compositeBuckets: Record<string, Cell[]> = {};
 
+    // To get it we rip through the first order buckets
+    // For each bucket we compare it with every other bucket, to find the composite bucket name we create a set of all the possible values (removing duplicates), and sort it numerically
+    // Example from above, the first first-order bucket will be 123, the second will be 12
+    // The composite bucket name will be 123, both cells are contained in this bucket
+    // The composite bucket of 123 and 2378? 12378, this bucket will also hold 12, and 23
+
+    // A single bucket will contain one or more cells, and any cell can be in many buckets
     const firstOrderBucketValues = Object.keys(firstOrderBuckets);
     for (let i = 0; i < firstOrderBucketValues.length; i++) {
+      // rip through all first order buckets, which will represent actual cells
       for (let j = i; j < firstOrderBucketValues.length; j++) {
+        // we need to compare it to every other first order bucket, we can reuse the index for a minor optimization, dont need to look back!
         const baseValueStr = firstOrderBucketValues[i];
         const otherValueString = firstOrderBucketValues[j];
         const compositeValueStr = Array.from(
@@ -147,10 +163,16 @@ export class Game {
       }
     }
 
+    // Now we have all our composite buckets we need to populate them
+    // I do this by looking at the first order buckets if the values in the FObucket are a strict subset of the values in the composite, we concat that bucket.
+    // remember, by values in the bucket, i am referring to the bucket 'name' which is the possible values sorted and joined '123' or '12'
+
     for (const compositeBucketValues of Object.keys(compositeBuckets)) {
       const compositeBucketValueArr = compositeBucketValues.split("");
       for (const firstOrderBucketValue of firstOrderBucketValues) {
         const firstOrderBucketValueArr = firstOrderBucketValue.split("");
+        // Create a new set from all the values of the two buckets
+        // If the new set's size is the same as the composite then the FO bucket contains no values that are not in the composite, add the cellls to the bucket
         if (
           new Set(compositeBucketValueArr).size ===
           new Set([...compositeBucketValueArr, ...firstOrderBucketValueArr])
@@ -162,6 +184,11 @@ export class Game {
         }
       }
     }
+
+    // Now the elimination
+    // Rip through the composite buckets
+    // If the bucket name is the same length as the number of cells, we can eliminate the values in that bucket name from every cell not in the bucket
+    // If this seems like magic, remember, a bucket name is all the possible values and the cells in that bucket are all of the cells that can be every of those values
 
     for (const [valueString, groupedCells] of Object.entries(
       compositeBuckets,
@@ -179,6 +206,13 @@ export class Game {
   }
 
   handleCanOnlyBeInGroup(cells: Cell[]) {
+    // This is the simpler of the elimination methods
+    // For each family of cells (same row, col, or box), if only one cell's possible values contains any given possible value we know that's the one
+    // Begin with a map of all values 0-9 as nulls
+    // Loop over the cells, for each cell, loop over the possible values
+    // If the maps key for that value is null ad the cell
+    // If there is already a cell then more than one cell in this family can be that value
+
     const uniquelyCanBe = cells.reduce(
       (acc, cell) => {
         const valueString = cell.valueString;
@@ -203,6 +237,9 @@ export class Game {
         "9": null,
       } as Record<string, null | Cell | false>,
     );
+
+    // If any of the values in the map are a cell then the cells value is the key
+
     for (const [value, cellNullOrFalse] of Object.entries(uniquelyCanBe)) {
       if (cellNullOrFalse instanceof Cell) {
         cellNullOrFalse.setValue(Number(value));
@@ -247,6 +284,11 @@ export class Game {
   }
 
   trialAndErrorIt(): string {
+    // At this point we aren't learning anything from the logic elimination, so lets guess
+    // Find the cells with only 2 degrees of freedom, and assume a value, then try to solve it
+    // If it throws an error, we know it's the other value, rinse and repeat.
+    // Doing the cells with the least possible values is a nice optimization, because at this point we have gone through the logical eliminations we should be able to quickly determine if a value is unsolvable
+    // When this returns a string, it will be the game string of a solved sudoko
     const gameString = this.getGameString();
     const possibleValuesList = gameString.split(",");
     let groupSize = 2;
@@ -265,8 +307,6 @@ export class Game {
             return game.getGameString();
           } catch (e) {
             // we just want to ignore errors
-            // eslint-disable-next-line @typescript-eslint/no-unused-vars
-            const err = e;
           }
         }
       }
